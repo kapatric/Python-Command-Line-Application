ecb9533749a59a0dfbb44b40f9e9f7512379339d
kapatric <khristopherpatrick@gmail.com>
2022-11-17 11:03:26 -0500

    finished creation

--- schema.sql
+++ schema.sql
@@ -4,14 +4,14 @@ DROP TABLE IF EXISTS addresses;
 CREATE TABLE contacts (
   id SERIAL PRIMARY KEY,
   name VARCHAR(100),
-  telephone_number INTEGER(10) UNIQUE,
-  address_id INTEGER
+  telephone_number BIGINT UNIQUE,
+  address_id INT
 );
 
 CREATE TABLE addresses (
-  street_number INTEGER,
+  street_number INT,
   street_name VARCHAR(100),
   city_name VARCHAR(100),
   state VARCHAR(2),
-  zipcode INTEGER(5)
+  zipcode INT
 );
